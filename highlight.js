239f033e27bdcd8597d64c68df2f42189801f427
satyaladdha2 <74101665+satyaladdha2@users.noreply.github.com>
2021-08-20 14:19:05 +0530

    Add files via upload

---
+++ highlight.js
@@ -0,0 +1,46 @@
+AFRAME.registerComponent("cursor-listener", {
+    schema: {
+      selectedItemId: { default: "", type: "string" },
+    },
+    init: function () {
+      this.handleMouseEnterEvents();
+      this.handleMouseLeaveEvents();
+    },
+    handlePlacesListState: function () {
+      const id = this.el.getAttribute("id");
+      const placesId = ["comic", "download", "images", "spiderman"];
+      if (placesId.includes(id)) {
+        const placeContainer = document.querySelector("#places-container");
+        placeContainer.setAttribute("cursor-listener", {
+          selectedItemId: id,
+        });
+        this.el.setAttribute("material", {
+          color: "#D76B30",
+          opacity: 1,
+        });
+      }
+    },
+    handleMouseEnterEvents: function () {
+      //Cursor 'mouseenter' Events
+      this.el.addEventListener("mouseenter", () => {
+        this.handlePlacesListState();
+      });
+    },
+    handleMouseLeaveEvents: function () {
+      //Cursor 'mouseleave' Events
+      this.el.addEventListener("mouseleave", () => {
+        const { selectedItemId } = this.data;
+        if (selectedItemId) {
+          const el = document.querySelector(`#${selectedItemId}`);
+          const id = el.getAttribute("id");
+          if (id == selectedItemId) {
+            el.setAttribute("material", {
+              color: "#0077CC",
+              opacity: 1,
+            });
+          }
+        }
+      });
+    },
+  });
+  
